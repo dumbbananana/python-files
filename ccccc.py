a3996d7828d3d1e702b852149d1567a6acb40a56
ashish <atimsina2021@gmail.com>
2026-05-27 00:35:30 +0545

    created a working api call for chatbot in python really happy huhuhuhu

--- ccccc.py
+++ ccccc.py
@@ -6,9 +6,11 @@ from groq import Groq
 
 client = Groq(api_key = "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")
 
+history= []
 def called():
      response=client.chat.completions.create(
      messages=[{"role":"user", "content":give}],
+     history.append("role":"user", "content":give)
     model="llama-3.3-70b-versatile",
 
     )
