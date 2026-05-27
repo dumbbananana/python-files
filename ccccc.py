3a5ac70736f88dd0b9967895cdb6bad1c981ead9
ashish <atimsina2021@gmail.com>
2026-05-27 14:57:58 +0545

    created a working api call for chatbot in python really happy huhuhuhu

--- ccccc.py
+++ ccccc.py
@@ -1,18 +1,27 @@
 from groq import Groq
 
-
 client = Groq(api_key="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")
 
+memory = []  # this stores chat history
+while True:
+    user_input = input("You: ")
 
-def function():
+    if user_input == "quit":
+        break
+
+    # 1. add user message to memory
+    memory.append({"role": "user", "content": user_input})
+
+    # 2. send full memory to Groq
     response = client.chat.completions.create(
-    messages=[{"role": "user", "content":one}],
-    model="llama-3.3-70b-versatile"
-
+        model="llama-3.3-70b-versatile",
+        messages=memory
     )
 
-    print(response.choices[0].message.content)
+    ai_reply = response.choices[0].message.content
 
+    # 3. print reply
+    print("AI:", ai_reply)
 
-one=input(":")
-function()
+    # 4. store AI response too
+    memory.append({"role": "assistant", "content": ai_reply})
