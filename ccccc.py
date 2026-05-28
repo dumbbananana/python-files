fa6dab3294e9d1b40fba8dd719289e32f47770c7
ashish <atimsina2021@gmail.com>
2026-05-28 22:44:36 +0545

    using dictionary

--- ccccc.py
+++ ccccc.py
@@ -1,27 +1,6 @@
-from groq import Groq
+try:
+    print(g+"1")
+    print("helllo")
 
-client = Groq(api_key="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")
-
-memory = []  # this stores chat history
-while True:
-    user_input = input("You: ")
-
-    if user_input == "quit":
-        break
-
-    # 1. add user message to memory
-    memory.append({"role": "user", "content": user_input})
-
-    # 2. send full memory to Groq
-    response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=memory
-    )
-
-    ai_reply = response.choices[0].message.content
-
-    # 3. print reply
-    print("AI:", ai_reply)
-
-    # 4. store AI response too
-    memory.append({"role": "assistant", "content": ai_reply})
+except NameError:
+    print("error")
