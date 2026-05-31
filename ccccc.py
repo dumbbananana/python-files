57d0212cc39288d80e467ccacec45d14a8510502
ashish <atimsina2021@gmail.com>
2026-05-31 17:18:14 +0545

    maths boy using the math module very easy peasy

--- ccccc.py
+++ ccccc.py
@@ -1,20 +1,17 @@
-from groq import  Groq
-x=0
-
-knowntopics = []
-while True:
-
- ask=input(" Add new topics : ")
- if ask in knowntopics:
-     print("already added")
- else:
-  knowntopics.append(ask)
-  print(knowntopics)
-  if ask =="/q":
-      break
+import math
+import math as m
 
 
 
-while x < len(knowntopics)-1:
- print(knowntopics[x])
- x=x+1
+def function():
+ return m.tan(number)
+ return m.log(number,base=10)
+
+
+
+
+
+
+
+number = int(input("enter numbre"))
+print(function())
