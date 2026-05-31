9d3f6f28806c0c6a3662408a4e4928bfc898840b
ashish <atimsina2021@gmail.com>
2026-05-31 19:13:51 +0545

    made a working dunctioning calculator didnt make the use of math module tho

--- ccccc.py
+++ ccccc.py
@@ -1,11 +1,45 @@
 import math as m
 
 def calcualte ():
-    if operation == '+':
-       pass
+    try:
+     if operation == "+":
+       print(f"{first + second}")
+     elif operation == '-':
+        print(f"{first - second}")
+     elif operation == '*':
+        print(f"{first * second}")
+     elif operation == '**':
+        print(f"{first ** second}")
+     elif operation == '/':
+        print(f"{first / second}")
+     elif operation == "%":
+        print(f"{first % second}")
+     else:
+        print(f"{operation} is not a valid operation provided\n")
+
+    except ZeroDivisionError:
+        print("cannot divide ero with zero, try again \n")
+
+
 
 print("avilable operations \n+\n-\n*\n**\n/\n%")
-operation = float(input("enter your operation"))
-first = int(input("enter your first number"))
-second = int(input("enter your second number"))
+print("/q to quit")
+while True:
+ operation = (input("enter your operation : "))
+ if operation == "/q":
+     break
+
+ elif operation not in ["+", "-", "*", "**", "/", "%"]:
+    print("operant not avilable \n")
+
+ else:
+     try:
+      first = int(input("enter your first number : "))
+      second = int(input("enter your second number : "))
+     except:
+      print("enter value")
+
+     else:
+         calcualte()
+
 
